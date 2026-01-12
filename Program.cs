2f93c0b70caf2f7c8777138b74678c3217d005dc
memetoleng91-dot <memetoleng91@gmail.com>
2026-01-12 09:51:29 +0700

    validasi nilai

--- Program.cs
+++ Program.cs
@@ -1,4 +1,18 @@
-﻿int nilai;
+﻿using System.ComponentModel.Design;
+
+int nilai;
 string nama;
 
 Console.WriteLine("Aplikasi Nilai Siswa");
+
+Console.Write("Input Nama: ");
+nama = Console.ReadLine();
+
+Console.Write("Input nilai: ");
+nilai = int.Parse(Console.ReadLine());
+
+if (nilai >= 75)
+    Console.WriteLine("selamat anda lulus");
+
+else
+    Console.WriteLine("maaf anda belum lulus");
